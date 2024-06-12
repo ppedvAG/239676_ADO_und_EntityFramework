12a2919ab169ad18012faa3fd883461ae60a4d42
André <AndreR@ppedv.de>
2024-06-12 11:31:49 +0200

    EfCore basic config

---
+++ YummyInMyTummy.Model/YummyInMyTummy.Data.EfCore/EfContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YummyInMyTummy.Model.Domain;
+
+namespace YummyInMyTummy.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public EfContext(DbContextOptions<EfContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+    }
+}
