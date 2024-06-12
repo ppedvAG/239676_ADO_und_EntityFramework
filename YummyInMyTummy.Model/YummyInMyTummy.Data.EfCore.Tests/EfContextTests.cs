72225d6f92d4c7e6e3d512d59912bc754c1b736f
André <AndreR@ppedv.de>
2024-06-12 12:23:06 +0200

    Tests

--- YummyInMyTummy.Model/YummyInMyTummy.Data.EfCore.Tests/EfContextTests.cs
+++ YummyInMyTummy.Model/YummyInMyTummy.Data.EfCore.Tests/EfContextTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.Contracts;
+using YummyInMyTummy.Model.Domain;
 
 namespace YummyInMyTummy.Data.EfCore.Tests
 {
@@ -17,12 +18,66 @@ namespace YummyInMyTummy.Data.EfCore.Tests
         [Fact]
         public void Can_create_Db()
         {
-            var con = CreateContext();  
+            var con = CreateContext();
             con.Database.EnsureDeleted();
 
             var result = con.Database.EnsureCreated();
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Can_add_Pizza()
+        {
+            var con = CreateContext();
+            con.Database.EnsureCreated();
+
+            var pizza = new Pizza() { Name = "Testpizza #1" };
+            con.Add(pizza);
+            var rows = con.SaveChanges();
+
+            Assert.Equal(2, rows);
+        }
+
+        [Fact]
+        public void Can_add_Pizza_with_Sizes()
+        {
+            var con = CreateContext();
+            con.Database.EnsureCreated();
+
+            var pizza = new Pizza() { Name = "Testpizza #2" };
+            pizza.OfferedSizes.Add(PizzaSize.r33cm);
+            pizza.OfferedSizes.Add(PizzaSize.r40cm);
+            con.Add(pizza);
+            var rows = con.SaveChanges();
+
+            Assert.Equal(2, rows);
+        }
+
+        [Fact]
+        public void Can_read_Pizza_with_Sizes()
+        {
+            var pizza = new Pizza() { Name = "Testpizza #3" };
+            pizza.OfferedSizes.Add(PizzaSize.r33cm);
+            pizza.OfferedSizes.Add(PizzaSize.r40cm);
+
+            using (var con = CreateContext())
+            {
+                con.Database.EnsureCreated();
+                con.Add(pizza);
+                con.SaveChanges();
+            }
+
+            using (var con = CreateContext())
+            {
+                var loaded = con.Find<Pizza>(pizza.Id);
+                Assert.NotNull(loaded);
+                Assert.Contains(PizzaSize.r33cm, pizza.OfferedSizes);
+                Assert.Contains(PizzaSize.r40cm, pizza.OfferedSizes);
+                Assert.DoesNotContain(PizzaSize.r28cm, pizza.OfferedSizes);
+                Assert.DoesNotContain(PizzaSize.s33x46cm, pizza.OfferedSizes);
+                Assert.DoesNotContain(PizzaSize.s60x40cm, pizza.OfferedSizes);
+           }
         }
     }
 }
--- YummyInMyTummy.Model/YummyInMyTummy.Model/Domain/Pizza.cs
+++ YummyInMyTummy.Model/YummyInMyTummy.Model/Domain/Pizza.cs
@@ -2,7 +2,20 @@
 
 public class Pizza : Food
 {
+    public List<PizzaSize> OfferedSizes { get; set; } = new List<PizzaSize>();
+
     public virtual ICollection<Topping> Toppings { get; set; } = new HashSet<Topping>();
 }
+
+public enum PizzaSize
+{
+    r28cm,
+    r33cm,
+    r40cm,
+    s33x46cm,
+    s60x40cm,
+}
+
+    
 
 
